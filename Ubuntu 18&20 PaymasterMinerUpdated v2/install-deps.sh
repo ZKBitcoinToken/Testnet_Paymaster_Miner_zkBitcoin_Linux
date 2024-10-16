33b609cdf7cb2994ee2d7d000f9e18d1d667410d
zkBitcoin Token <128942687+ZKBitcoinToken@users.noreply.github.com>
2024-10-16 16:28:13 -0400

    Add files via upload

---
+++ Ubuntu 18&20 PaymasterMinerUpdated v2/install-deps.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+rm -f packages-microsoft-prod.deb
+
+
+ubuntu_version=$(lsb_release -rs)
+
+# Proceed with the download for the detected Ubuntu version
+echo "Ubuntu $ubuntu_version detected. Proceeding with the download..."
+wget https://packages.microsoft.com/config/ubuntu/${ubuntu_version}/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb   sudo dpkg -i packages-microsoft-prod.deb
+
+sudo dpkg -P packages-microsoft-prod
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+
+rm -f packages-microsoft-prod.deb
+
+sudo apt-get install apt-transport-https -y
+sudo apt-get install dotnet-sdk-5.0 -y
+sudo ubuntu-drivers autoinstall
+
