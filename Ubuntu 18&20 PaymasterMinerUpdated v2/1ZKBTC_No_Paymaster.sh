2437adde74239adb55426326a25ef22840b8a8d5
zkBitcoin Token <128942687+ZKBitcoinToken@users.noreply.github.com>
2024-10-21 19:09:06 -0500

    Update 1ZKBTC_No_Paymaster.sh

--- Ubuntu 18&20 PaymasterMinerUpdated v2/1ZKBTC_No_Paymaster.sh
+++ Ubuntu 18&20 PaymasterMinerUpdated v2/1ZKBTC_No_Paymaster.sh
@@ -6,15 +6,19 @@ if ! command -v dotnet &> /dev/null
 then
     echo "dotnet 5.0 is not found or not installed."
     echo "Installing dotnet 5.0..."
+    ubuntu_version=$(lsb_release -rs)
+    if [[ "$ubuntu_version" == "22.04" || "$ubuntu_version" == "24.04" || "$ubuntu_version" == "20.04" ]]; then
+        wget 'http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb'
+        
+        sudo dpkg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
+
+        target_version="20.04"
+    else
+        target_version="$ubuntu_version"
+    fi
 
     # Add Microsoft package signing key and repository
-
-    # Get the Ubuntu version
-    ubuntu_version=$(lsb_release -rs)
-
-    # Proceed with the download for the detected Ubuntu version
-    echo "Ubuntu $ubuntu_version detected. Proceeding with the download..."
-    wget https://packages.microsoft.com/config/ubuntu/${ubuntu_version}/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+    wget https://packages.microsoft.com/config/ubuntu/$target_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
     sudo apt-get update; \
     sudo apt-get install -y apt-transport-https && \
